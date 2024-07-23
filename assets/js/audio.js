8e2dcd128184536a1a179d77a23a56ead9e94c00
JorgeLReyes <JorgeReyes.130101@gmail.com>
2024-07-23 02:32:30 -0600

    Bug fixed?

--- assets/js/audio.js
+++ assets/js/audio.js
@@ -1,8 +1,9 @@
 export default () => {
   document.addEventListener("click", (e) => {
-    document.querySelector("audio").play();
+    // document.querySelector("audio").play();
     // document.querySelector("audio").loop();
     if (e.target.closest(".container-song")) {
+      document.querySelector("audio").play();
       document.querySelector(".audio").classList.toggle("active");
     }
   });

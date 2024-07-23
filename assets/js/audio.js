2043805024519bf51c9f109766d3b540fd7fa52b
JorgeLReyes <JorgeReyes.130101@gmail.com>
2024-07-23 02:19:05 -0600

    Add audio

--- assets/js/audio.js
+++ assets/js/audio.js
@@ -1,5 +1,7 @@
 export default () => {
   document.addEventListener("click", (e) => {
+    document.querySelector("audio").play();
+    document.querySelector("audio").loop();
     if (e.target.closest(".container-song")) {
       document.querySelector(".audio").classList.toggle("active");
     }
